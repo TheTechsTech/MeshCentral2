659aeaa27425d2af778a7e389779ae206c0f94e1
techno-express <technoexpressnet@gmail.com>
2018-10-24 17:26:32 -0400

    updating

--- README.md
+++ README.md
@@ -10,3 +10,34 @@ docker run --name meshcentral \
 -e DB=netdb -e MONGODB="mongodb://127.0.0.1:27017/meshcentral" -e MONGODBCOL="meshcentral" \
 --restart=always --hostname=mesh.host.name -d technoexpress/meshcentral2
 ```
+
+Additionally passing the following environment options at run time controls operations:
+```
+`–e PORT=443 `
+`–e REDIRPORT=80`
+`–e MPSPORT=4443`
+`–e EMAIL=mail@`
+`–e HOST=host.ltd`
+`–e SMTP=smtp.host.ltd`
+`–e USER=smtp@user`
+`–e PASS=smtppass!`
+`–e DB=netdb`
+`–e MONGODB="mongodb://127.0.0.1:27017/meshcentral"`
+`–e MONGODBCOL="meshcentral"`
+```
+
+If nothing is passed i,n the above are defaults, and USER, HOST, and EMAIL change to support@ detected docker HOSTNAME, and no password.
+
+
+## This build also assume reverse proxy is setup. This build setup uses https://github.com/adi90x/rancher-active-proxy
+
+```
+-v /nginx/rancher-active-proxy/letsencrypt/archive/mesh.host.name:/etc/letsencrypt/archive/mesh.host.name \
+-l rap.host=mesh.host.name \
+-l rap.le_host=mesh.host.name \
+-l rap.https_method=noredirect \
+```
+
+# Docker Hub
+
+https://hub.docker.com/r/technoexpress/meshcentral2/builds/ automatically builds the latest changes into images which can easily be pulled and ran with a simple docker run command. 
--- startmeshcentral.sh
+++ startmeshcentral.sh
@@ -19,27 +19,32 @@ sed -i "s#: 443,#: $PORT,#" meshcentral-data/config.json
 sed -i "s#: 80,#: $REDIRPORT,#" meshcentral-data/config.json
 sed -i "s#: 4443,#: $MPSPORT,#" meshcentral-data/config.json
 
-if [ "$EMAIL" != 'mail@' ]; then
+if [ "$EMAIL" != 'mail@' ]
+then
     sed -i "s#\"email\": \"mail@\",#\"email\": \"$EMAIL\",#/g" meshcentral-data/config.json
 else
     sed -i "s#\"email\": \"mail@\",#\"email\": \"support@$HOSTNAME\",#/g" meshcentral-data/config.json
 fi
 
-if [ "$HOST" != 'host.ltd' ] && [ "$SMTP" != 'smtp.host.ltd' ]; then
+if [ "$HOST" != 'host.ltd' ] && [ "$SMTP" != 'smtp.host.ltd' ]
+then
     sed -i "s#\"names\": \"host.ltd,smtp.host.ltd\",#\"names\": \"$HOST,$SMTP\",#" meshcentral-data/config.json
 elif [ "$HOST" != 'host.ltd' ]
+then
     sed -i "s#\"names\": \"host.ltd,smtp.host.ltd\",#\"names\": \"$HOST\",#" meshcentral-data/config.json
 else
     sed -i "s#\"names\": \"host.ltd,smtp.host.ltd\",#\"names\": \"$HOSTNAME\",#" meshcentral-data/config.json
 fi
  
-if [ "$SMTP" != 'smtp.host.ltd' ]; then
+if [ "$SMTP" != 'smtp.host.ltd' ]
+then
     sed -i "s#\"host\": \"smtp.host.ltd\",#\"host\": \"$SMTP\",#" meshcentral-data/config.json
 else
     sed -i "s#\"host\": \"smtp.host.ltd\",#\"host\": \"$HOSTNAME\",#" meshcentral-data/config.json    
 fi
 
-if [ "$USER" != 'smtp@user' ] || [ "$PASS" != 'smtppass!' ]; then
+if [ "$USER" != 'smtp@user' ] || [ "$PASS" != 'smtppass!' ]
+then
     sed -i "s#\"user\": \"smtp@user\",#\"user\": \"$USER\",#" meshcentral-data/config.json
     sed -i "s#\"pass\": \"smtppass!\",#\"pass\": \"$PASS\",#" meshcentral-data/config.json
 else
@@ -47,8 +52,9 @@ else
     sed -i "s#\"pass\": \"smtppass!\",#\"pass\": \"\",#" meshcentral-data/config.json  
 fi
 
-if [ "$DB" != "netdb" ]; then
-    if ! [ -f mongodbready ];then
+if [ "$DB" != "netdb" ]
+    if ! [ -f mongodbready ]
+	then
         sed -i "s#\"settings\": {#\"settings\": {\n\t\"MongoDb\": \"$MONGODB\",\n\t\"MongoDbCol\": \"$MONGODBCOL\",#" meshcentral-data/config.json   
         node meshcentral --dbexport
         node meshcentral --mongodb mongodb://127.0.0.1:27017/meshcentral --dbimport
@@ -57,7 +63,8 @@ if [ "$DB" != "netdb" ]; then
     service mongod start
 fi
 
-if [ -f "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" ]; then
+if [ -f "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" ]
+then
     ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/agentserver-cert-private.key  
     ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/agentserver-cert-public.crt
     ln -sf "/etc/letsencrypt/archive/$HOSTNAME/privkey1.pem" meshcentral-data/root-cert-private.key   
@@ -68,7 +75,8 @@ if [ -f "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" ]; then
     ln -sf "/etc/letsencrypt/archive/$HOSTNAME/cert1.pem" meshcentral-data/mpsserver-cert-public.crt
 fi
 
-if ! [ -f meshcentral-data/agentserver-cert-private.key ]; then 
+if ! [ -f meshcentral-data/agentserver-cert-private.key ]
+then 
 	node node_modules/meshcentral/meshcentral.js --cert $HOSTNAME
 else 
 	node node_modules/meshcentral/meshcentral.js
